cd971cdba44a55af351b36a6e797f3f7555339b7
ManuelWestermeier <westermeier111@gmail.com>
2024-12-07 12:33:03 +0100

    add basic ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,25 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import DesktopNavbar from "./components/desktop-navbar";
+import MobileNavbar from "./components/mobile-navbar";
+import Home from "./pages/home";
+import Search from "./pages/search";
+import Subscriptions from "./pages/subscriptions";
+import Profile from "./pages/profile";
+import Create from "./pages/create";
+
 export default function App() {
   return (
-    <div>App</div>
-  )
+    <div>
+      <DesktopNavbar />
+      <MobileNavbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/subscriptions" element={<Subscriptions />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/create" element={<Create />} />
+      </Routes>
+    </div>
+  );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import { HashRouter } from "react-router-dom";
+import { createRoot } from "react-dom/client";
+import { StrictMode } from "react";
+import App from "./app.jsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </StrictMode>
+);
